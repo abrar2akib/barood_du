27abdf58d49602b726bcb72415be3a9d5d38aa34
abrar2akib <abrarakib242@gmail.com>
2022-02-01 01:51:00 +0600

    modal added

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(window).on('load', function() {
+    $('#exampleModalCenter').modal('show');
+});
+$(document).ready(function () {
+    $(".collapse").on("shown.bs.collapse", function () {
+        localStorage.setItem("coll_" + this.id, true);
+    });
+
+    $(".collapse").on("hidden.bs.collapse", function () {
+        localStorage.removeItem("coll_" + this.id);
+    });
+
+    $(".collapse").each(function () {
+            if (localStorage.getItem("coll_" + this.id) === "true") {
+                $(this).collapse("show");
+            }
+            else {
+                $(this).collapse("hide");
+            }
+    });
+});
